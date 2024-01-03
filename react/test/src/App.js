9bd1322264cab77a08e52d533c0b187f5fe0f435
myj1217 <myj1217@naver.com>
2024-01-03 17:20:21 +0900

    Update App.js

--- react/test/src/App.js
+++ react/test/src/App.js
@@ -10,8 +10,8 @@ const App = () => {
       .get(
         "http://data4library.kr/api/libSrch?authKey=b85ec318ffca5a5f63a9fcf1e0a6cc95f00eda54e322fdb26fafe700420c33c5&pageNo=1&pageSize=10&format=json"
       )
+      // api를 불러오는 데 성공할 경우 = resolve = then
       .then((response) => {
-        // 성공 = resolve = then
         setData(response.data);
       });
   };
@@ -20,7 +20,8 @@ const App = () => {
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
-      {data && ( // data가 true면 실행하라
+      {/* data가 true면 실행하라 */}
+      {data && (
         <textarea
           rows={7}
           value={JSON.stringify(data, null, 2)}
