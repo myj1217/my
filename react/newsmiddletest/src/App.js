dac18bda93e242feacf70b34600138deecbce356
myj1217 <myj1217@naver.com>
2024-01-05 11:35:38 +0900

    Add Highlight

--- react/newsmiddletest/src/App.js
+++ react/newsmiddletest/src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import NewsSearch from "./components/NewsSearch";
 import Categories from "./components/Categories";
 
 const App = () => {
+  const [category, setCategory] = useState("search");
+  const onSelect = useCallback((category) => setCategory(category), []);
   return (
     <div>
-      <Categories />
+      <Categories category={category} onSelect={onSelect} />
       <NewsSearch />
     </div>
   );
